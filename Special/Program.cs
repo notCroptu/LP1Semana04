62faf08a82b431e967370f5cf21855fa99021791
notCroptu <martinsmariana1972@gmail.com>
2024-03-13 18:59:46 +0000

    Ex4 Sequencia especial

---
+++ Special/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Special
+{
+    class Program
+    {
+        private static int GetSpecial( int n)
+        {
+            int result;
+            if (n == 0)
+            {
+                result = 0;
+            }
+            else if (n == 1)
+            {
+                result = 1;
+            }
+            else
+            {
+                result = GetSpecial(n - 1) + (2*GetSpecial(n - 2));
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Insert n-th number of the special sequence: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetSpecial(n));
+        }
+    }
+}
